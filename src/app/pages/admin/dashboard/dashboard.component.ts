07137820e718085cae35d7ab17f6f9ccbcdcfbe0
Andrea Zapata <andrea.zapata@estudiantat.upc.edu>
2026-06-21 09:19:32 +0200

    primer commit

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +9,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  grafanaUrl!: SafeResourceUrl;
+  private sanitizer = inject(DomSanitizer);
+
+  ngOnInit() {
+    const rawUrl = 'http://localhost:3000/d-solo/ad4g45h/new-dashboard?orgId=1&timezone=browser&panelId=panel-1';
+    // Le decimos a Angular que confíe en esta URL para usarla en un iframe
+    this.grafanaUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
+  }
+}
