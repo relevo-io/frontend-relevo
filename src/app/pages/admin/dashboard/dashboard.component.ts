6c4198e36f36cfd963d9ad1d9480d67a122bd921
Andrea Zapata <andrea.zapata@estudiantat.upc.edu>
2026-06-21 09:42:43 +0200

    panel2

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -10,12 +10,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  grafanaUrl!: SafeResourceUrl;
+  grafanaSectorUrl!: SafeResourceUrl;
+  grafanaRevenueUrl!: SafeResourceUrl;
   private sanitizer = inject(DomSanitizer);
 
   ngOnInit() {
     const rawUrl = 'http://localhost:3000/d-solo/ad4g45h/new-dashboard?orgId=1&timezone=browser&panelId=panel-1';
     // Le decimos a Angular que confíe en esta URL para usarla en un iframe
-    this.grafanaUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
+    this.grafanaSectorUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
+
+    const revenueRawUrl = 'http://localhost:3000/d-solo/ad4g45h/new-dashboard?orgId=1&timezone=browser&panelId=panel-2';
+    this.grafanaRevenueUrl = this.sanitizer.bypassSecurityTrustResourceUrl(revenueRawUrl);
   }
 }
